b46f0802391cb648edb8f493c9fa2f37426dd653
iyunakov <76445770+yunikeil@users.noreply.github.com>
2023-03-07 18:01:40 +0300

    -

--- games/MazeGame/data/engine.py
+++ games/MazeGame/data/engine.py
@@ -5,19 +5,17 @@ import re
 filename = r"data\levels\game.md"
 dir_path = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(dir_path, filename)
-data = ""
 
 
-def split_string_with_separator(string, separator_regex):
-    result = []
-    for match in re.finditer(separator_regex, string):
-        
-    return result
+data = []
 
-
+def 
 with open(file_path, "r", encoding="utf-8") as file:
     data = file.read().replace("\n\n", "\n")
-    data = split_string_with_separator(data, r"(\#\#\s\[.*?\]\(\#+[0-9]*\))")
+    block_names = re.findall(r"(\#\#\s\[.*?\]\(\#+[0-9]*\))", data)
+    for line in data.split('\n'):
+        block_indexes
+
 
 
 
