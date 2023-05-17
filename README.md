f49972c90ec86c05279d76d9d77e58521b6834a5
iyunakov <76445770+yunikeil@users.noreply.github.com>
2023-05-17 14:37:04 +0300

    -

--- README.md
+++ README.md
@@ -39,3 +39,11 @@
     * Практическое занятие:
     [gh](https://github.com/true-grue/kispython/blob/main/pract5.ipynb)
     [co](https://colab.research.google.com/github/true-grue/kispython/blob/main/pract5.ipynb)
+
+1. Функциональное программирование
+
+    * Лекция:
+    [co](https://colab.research.google.com/github/true-grue/kispython/blob/main/lect6.ipynb)
+    * Практическое занятие:
+    [gh](https://github.com/true-grue/kispython/blob/main/pract6.ipynb)
+    [co](https://colab.research.google.com/github/true-grue/kispython/blob/main/pract6.ipynb)
--- solved/pract_git/pract5_2.1/testing.py
+++ solved/pract_git/pract5_2.1/testing.py
@@ -1,3 +1,5 @@
+import datetime
+
 class User:
     def __init__(self, login, password, role):
         self.login = login
@@ -98,39 +100,52 @@ class Task:
 
 
 def test_objects():
-    user = User(
-        login="yunik",
-        password="123321",
-        role="student"
-    )
-    user.authenticate("yunik", "123321")
-    user.authenticate("qwerty", "45678")
+    user = User("yunikeil", "123321", "student")
 
-    user.authorize(["student"])
-    user.authorize(["teacher"])
+    assert user.authenticate("yunikeil", "123321") == True
+    assert user.authenticate("yunikeil", "321123") == False
+
+    assert user.authorize("student") == True
+    assert user.authorize("stuudent") == False
+
+    project = Project("user project", "projdesc", "24.06.2021", "24.12.2021")
+
+    task1 = Task("task1", "task1desc", "10.07.2021", "28.08.2021")
+    task2 = Task("task2", "task2desc", "19.12.2021", "24.12.2021")
+
+    task1.change_status()
+    assert task1.status == True
+
+    task1.add_performer("yunikeil")
+    assert task1.performers == ["yunikeil"]
+
+    task1.remove_performer("yunikeil")
+
+    assert task1.performers == []
+    assert task1.to_dict() == {
+        'name': "task1",
+        'description': "task1desc",
+        'start_date': "10.07.2021",
+        'end_date': "28.08.2021",
+        'status': True,
+        'performers': []
+    }
+    x = task1.to_dict()
+    assert task1.from_dict(x)
+
+    project.add_task(task1)
+    project.add_task(task2)
+
+    assert project.find_task_by_name("task1")
+    assert project.find_task_by_name("task2")
+
+    assert project.find_task_by_start_date('10.07.2021')
+    assert project.find_task_by_end_date('28.08.2021')
+
+    dict = project.to_dict()
+    assert project.from_dict(dict)
     
-    task = Task()
-
-    proj = Project(
-        name="Gaembot",
-        description="Game bot programm",
-        start_date=10,
-        end_date=15
-    )
-    proj.add_task("task1")
-    proj.remove_task("task1")
-
-    proj.find_task_by_name("task1")
-    proj.find_task_by_start_date(10)
-    proj.find_task_by_end_date(15)
-
-    proj.is_completed()
-
-    #proj.add_task("task2")
-    dict_proj = proj.to_dict()
-    proj = proj.from_dict(dict_proj)
-
-
-
+    project.remove_task(task2)
+    assert project.is_completed()
 
 
